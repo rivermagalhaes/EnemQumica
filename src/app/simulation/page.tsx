c6dc8150afeedfd3027376ef473097e7a33bc9da
River <rivermagalhaes@gmail.com>
2026-01-31 23:05:33 -0300

    fix(build): replace next build with static copy and remove vestigial src

--- src/app/simulation/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ENEMSimulator } from '@/components/simulation/ENEMSimulator'
-
-export default function SimulationPage() {
-    return (
-        <div className="min-h-screen">
-            <ENEMSimulator />
-        </div>
-    )
-}
